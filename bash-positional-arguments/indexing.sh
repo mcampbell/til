9cd2b7a70505a67a7f98d9c71b5a6b030e8094b5
Michael Campbell <michael.campbell@gmail.com>
2025-05-04 08:42:26 -0400

    added space to check significance

--- bash-positional-arguments/indexing.sh
+++ bash-positional-arguments/indexing.sh
@@ -9,3 +9,6 @@ echo First arg: \[${@: 1:1}\] # arg is 1-indexed; program file is the 0'th
 # You can index from the end
 echo Last arg: \[${@: -1:1}\]
 echo Last arg: \[${@: -1}\]  # Seems to default to 1 length/size
+
+# The space is significant
+echo Last arg: \[${@:-1:1}\]
